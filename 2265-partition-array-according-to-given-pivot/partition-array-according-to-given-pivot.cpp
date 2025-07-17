cbc6b4a80f0b4261712b02c43741d3cfaa8eba2a
LOHITHA DAMISETTI <23mh1a4413@acoe.edu.in>
2025-07-17 20:04:47 +0530

    Time: 11 ms (29.69%) | Memory: 140.9 MB (16.91%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         vector<int> less;
+        vector<int> equal;
         vector<int> great;
         for(int n:nums){
             if(n<pivot){
                 less.push_back(n);
             }
-            if(n>pivot){
+           else if(n>pivot){
                 great.push_back(n);
             }
+            else{
+                equal.push_back(n);
+            }
         }
-        int k=nums.size()-(less.size()+great.size());
-        while(k--){
-            less.push_back(pivot);
-        }
-        for(int n:great){
-                less.push_back(n);
-        }
-        return less;
+       vector<int> res;
+       res.insert(res.end(),less.begin(),less.end());
+       res.insert(res.end(),equal.begin(),equal.end());
+       res.insert(res.end(),great.begin(),great.end());
+       return res;
     }
 };
