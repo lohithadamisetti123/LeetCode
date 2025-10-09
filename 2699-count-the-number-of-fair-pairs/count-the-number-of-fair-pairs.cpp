8be349d91e7ad50a41031d4386301e02b3dcd7f7
LOHITHA DAMISETTI <23mh1a4413@acoe.edu.in>
2025-10-09 23:23:06 +0530

    Time: 67 ms (56.12%) | Memory: 71.7 MB (42.29%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        long long res=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            auto left  = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]);
+            auto right = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]);
+            res += (right - left);
+
+        }
+        return res;
+    }
+};
