6f87f3f131f21562e1402bedf15eda4c48096e2a
LOHITHA DAMISETTI <23mh1a4413@acoe.edu.in>
2025-10-21 22:33:40 +0530

    Time: 4 ms (76.48%) | Memory: 45.5 MB (6.61%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static boolean anagram(String s1,String s2){
+        char[] c1=s1.toCharArray();
+        char[] c2=s2.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        return Arrays.equals(c1,c2);
+    }
+    public boolean isAnagram(String s, String t) {
+        if(anagram(s,t)){
+            return true;
+        }
+        return false;
+    }
+}
