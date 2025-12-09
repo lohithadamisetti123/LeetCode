86fd9853023637299be56afc8415e517c1348b26
LOHITHA DAMISETTI <23mh1a4413@acoe.edu.in>
2025-12-09 21:43:33 +0530

    Time: 59 ms (4.19%) | Memory: 31.4 MB (7.59%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -6,12 +6,17 @@ public:
             freq[i]++;
         }
         int res=0;
-        for(int i:nums){
-            if(freq[i]==1){
-                res=i;
-                break;
-            }
-        }
-        return res;
+        // for(int i:nums){
+        //     if(freq[i]==1){
+        //         res=i;
+        //         break;
+        //     }
+        // }
+        // return res;
+        sort(nums.begin(), nums.end(), [&](int a, int b){
+        if(freq[a] == freq[b]) return a > b; 
+        return freq[a] < freq[b];             
+         });
+         return nums[0];
     }
 };
